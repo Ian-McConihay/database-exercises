f730f32d2daf9e042b118af349ca7a1ec7444d08
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-17 09:09:33 -0600

    Think I got it

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_test_dp;
+    CREATE TABLE IF NOT EXISTS quotes (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist_name VARCHAR(40) DEFAULT 'NONE',
+    record_name  VARCHAR(50) NOT NULL,
+    release_date INT UNSIGNED,
+    sales_mil FLOAT,
+    genre TEXT NOT NULL,
+    PRIMARY KEY (id)
+    );
--- lbums_migration.sql
+++
@@ -1,8 +0,0 @@
-USE codeup_test_dp;
-CREATE TABLE If NOT EXISTS quotes (
-                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                        author_first_name VARCHAR(50) DEFAULT 'NONE',
-                        author_last_name  VARCHAR(100) NOT NULL,
-                        content TEXT NOT NULL,
-                        PRIMARY KEY (id)
-);
