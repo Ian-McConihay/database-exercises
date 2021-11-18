aea75eac2a73ebdbb99454b3c5e58b9d1902b2cb
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-18 14:17:49 -0600

    LIKE E OR  LIKE E

--- where_exercises.sql
+++ where_exercises.sql
@@ -21,3 +21,11 @@ WHERE first_name = 'Irena' OR first_name = 'Vidya' OR  first_name ='Maya';
 
 SELECT * FROM employees
 WHERE first_name = 'Irena' OR first_name = 'Vidya' OR  first_name ='Maya' OR gender = 'M';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+
+
+
+
