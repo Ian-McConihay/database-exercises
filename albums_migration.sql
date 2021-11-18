de1d6b703d2fdf8798f14bf1fac9bcda07ef5d91
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-18 10:23:09 -0600

    albums select

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,5 +1,5 @@
 USE codeup_test_dp;
-    DROP TABLE IF IT EXIST quotes;
+    DROP TABLE IF EXISTS albums;
     CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist_name VARCHAR(50) NOT NULL DEFAULT 'NONE',
@@ -9,3 +9,8 @@ USE codeup_test_dp;
     genre VARCHAR(50),
     PRIMARY KEY (id)
     );
+-- mysql -u root -p -t < albums_seeder.sql
+-- mysql -u root -p -t < albums_migration.sql
+--
+-- codeup_test_user
+--
