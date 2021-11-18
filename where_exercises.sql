9b719846220fdc9260a9672d6f8a46f6cf82d6e4
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-18 14:03:06 -0600

    employees last name starts with E

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,12 @@
 USE employees;
+# % is like a break for search. so to find batman '%man' or 'bat%'
+
+
+
+
 
 SELECT * FROM employees
 WHERE first_name IN ('Irena', 'Vidya' , 'Maya');
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%';
