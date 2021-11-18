633b7e63409892022f1e0ac73a524e148c31964d
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-18 14:10:33 -0600

    Part 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -10,3 +10,11 @@ WHERE first_name IN ('Irena', 'Vidya' , 'Maya');
 
 SELECT * FROM employees
 WHERE last_name LIKE 'E%';
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%';
+
+
+# Part 2
+SELECT * FROM employees
+WHERE first_name IN ('Irena') OR 'Vidya' OR 'Maya';
