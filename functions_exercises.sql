1f330c0b46c53159e4459b62106a375581ec42d6
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-19 10:56:06 -0600

    last one

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,7 +1,7 @@
 USE employees;
-SELECT CONCAT(first_name, ' ', last_name)
-AS 'FULL EMPLOYEE NAME'
-FROM employees LIMIT 10 offset 10;
+# SELECT CONCAT(first_name, ' ', last_name)
+# AS 'FULL EMPLOYEE NAME'
+# FROM employees LIMIT 10 offset 10;
 
 #2
 SELECT CONCAT(first_name, ' ', last_name)
@@ -14,19 +14,25 @@ ORDER BY last_name LIKE '%E';
 SELECT *
 FROM employees
 WHERE  month(birth_date) = 12
-  AND day(birth_date) = 25;
+AND day(birth_date) = 25;
 
 #4
 SELECT *
 FROM employees
 WHERE year(hire_date) BETWEEN 1990 AND 1999
-  AND month(birth_date) = 12
-  AND day(birth_date) = 25;
+AND month(birth_date) = 12
+AND day(birth_date) = 25;
 
 #5
 SELECT *
 FROM employees
 WHERE year(hire_date) BETWEEN 1990 AND 1999
-  AND month(birth_date) = 12
-  AND day(birth_date) = 25
-ORDER BY ;
+AND month(birth_date) = 12
+AND day(birth_date) = 25
+ORDER BY birth_date, hire_date DESC ;
+
+#6
+
+
+
+
