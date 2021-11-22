a61e20ffcb2f7a612ae5b9c16b9d60f2986b6480
Ian McConihay <mcconihay.ian92@gmail.com>
2021-11-22 14:46:47 -0600

    my sql join ex

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS join_test_dp;
+CREATE DATABASE join_test_dp;
     USE join_test_db;
 
 CREATE TABLE roles (
@@ -29,23 +29,48 @@ INSERT INTO users (name, email, role_id) VALUES
             ('jane', 'jane@example.com', null),
             ('mike', 'mike@example.com', null);
 
-SELECT * FROM users;
-SELECT * FROM roles;
+# SELECT * FROM users;
+# SELECT * FROM roles;
+#
+# # Inner Join shows all except null
+# SELECT users.name as user_name, roles.name as role_name
+# FROM users
+# JOIN roles ON users.role_id = roles.id;
+#
+# # left Join shows all
+# SELECT users.name AS user_name, roles.name AS role_name
+# FROM users
+# LEFT JOIN roles ON users.role_id = roles.id;
+#
+# #  Right join not common shows the roles_id and who they are attached to
+# SELECT users.name AS user_name, roles.name AS role_name
+# FROM users
+# RIGHT JOIN roles ON users.role_id = roles.id;
+
+#
+# Exercises
 
 
-SELECT users.name as user_name, roles.name as role_name
-FROM users
-JOIN roles ON users.role_id = roles.id;
 
 
+# Search department name and display it as DEPARTMENT_NAME -----
+# then CONCAT the first name and the last and display it under DEPARTMENT_MANAGER
 
+# SELECT d.dept_name AS DEPARTMENT_NAME, CONCAT(e.first_name, ' ', e.last_name) AS DEPARTMENT_MANAGER
 
+# Then go into departments and call it d
 
-
-
-
-
-
+# FROM departments as d
+#          JOIN dept_manager dm on d.dept_no = dm.dept_no
+#          JOIN employees e on dm.emp_no = e.emp_no
+# WHERE dm.to_date = '9999-01-01';
+SELECT d.dept_name AS DEPARTMENT_NAME, CONCAT(e.first_name, ' ', e.last_name) AS DEPARTMENT_MANAGER
+FROM departments as d
+         JOIN dept_manager as dm
+              on d.dept_no = dm.dept_no
+         JOIN employees as e
+              on dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01';
 
 
 
